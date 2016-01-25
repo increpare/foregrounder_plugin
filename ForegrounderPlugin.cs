8ab7da631e50a881484ac829c36bd615081014e4
Stephen Lavelle <analytic@gmail.com>
2016-01-25 10:11:24 +0000

    initial push

---
+++ ForegrounderPlugin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+using System.Runtime.InteropServices;
+#endif
+
+public class ForegrounderPlugin : MonoBehaviour {
+    bool first=true;
+
+    #if UNITY_STANDALONE_OSX
+
+    [DllImport ("Foregroundr")]
+    private static extern void activateWindow();
+
+	// Update is called once per frame
+    void Update () {
+        if (first)
+        {
+            first = false;
+            print("activating");
+            activateWindow();
+        }
+	}
+
+    #endif
+}
